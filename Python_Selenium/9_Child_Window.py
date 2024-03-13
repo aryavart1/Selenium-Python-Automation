59a4b05148e570243dd74aab4f0d358d92ba85f6
Aryavart <mailaryavart@gmail.com>
2024-03-13 06:02:34 +0530

    updated Python_Selenium

--- Python_Selenium/9_Child_Window.py
+++ Python_Selenium/9_Child_Window.py
@@ -15,4 +15,39 @@ driver = webdriver.Chrome(options=options, service=service_obj)
 
 driver.maximize_window()
 driver.get("https://the-internet.herokuapp.com/windows")
-driver.find_element((By.LINK_TEXT, "Click Here")).click()
+driver.find_element(By.LINK_TEXT, "Click Here").click()
+driver.implicitly_wait(2)
+
+# 'Window_handle' returns all the windows in the same order they are opened in the form of list.
+windows_opened = driver.window_handles
+
+# Locator used -> Tag name
+driver.switch_to.window(windows_opened[1])
+print(driver.find_element(By.TAG_NAME, "h3").text)
+driver.close()  # closes the window
+
+# Locator used -> Tag name
+driver.switch_to.window(windows_opened[0])
+assert "Opening a new window" == driver.find_element(By.TAG_NAME, "h3").text
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
