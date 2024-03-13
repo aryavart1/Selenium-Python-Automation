50ebd8544396914a4f7c1f41fa37ac5375f2259d
Aryavart <mailaryavart@gmail.com>
2024-03-14 02:28:27 +0530

    added 10_Child_Window_2.py

---
+++ Python_Selenium/10_Child_Window_2.py
@@ -0,0 +1,32 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+service_obj = Service()
+driver = webdriver.Chrome(options=options, service=service_obj)
+driver.implicitly_wait(4)
+
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/loginpagePractise/")
+driver.find_element(By.CSS_SELECTOR, ".blinkingText").click()
+windowsOpened = driver.window_handles
+
+driver.switch_to.window(windowsOpened[1])
+message = driver.find_element(By.CSS_SELECTOR, ".red").text
+var = message.split("at")[1].strip().split(" ")[0]
+driver.close()
+
+driver.switch_to.window(windowsOpened[0])
+driver.find_element(By.ID, "username").send_keys(var)
+driver.find_element(By.ID, "password").send_keys(var)
+driver.find_element(By.CSS_SELECTOR, "#signInBtn").click()
+wait = WebDriverWait(driver,10)
+wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".alert-danger")))
+print(driver.find_element(By.CSS_SELECTOR, ".alert-danger").text)
