25868d2a054525bf76df6dfbd71ec946a8ad4e91
Aryavart <mailaryavart@gmail.com>
2024-03-21 00:47:56 +0530

    added 12_Scroll.py

--- Python_Selenium/12_scroll.py
+++ Python_Selenium/12_scroll.py
@@ -1,14 +1,27 @@
-from selenium import webdriver
-from selenium.webdriver.support.select import Select
+""" Scrolling with JavaScript executor as Webdriver doesn't include that functionality """
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+service_obj = Service()
+driver = webdriver.Chrome(options=options, service=service_obj)
+
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+
+# To scroll to the bottom of the page
+driver.execute_script("window.scrollBy(0,document.body.scrollHeight);")
+
+"""  
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("headless")
 chrome_options.add_argument("--ignore-certificate-errors")
 
-
-driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe",options=chrome_options)
-
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 
-print(driver.title)
+print(driver.title) 
+"""
