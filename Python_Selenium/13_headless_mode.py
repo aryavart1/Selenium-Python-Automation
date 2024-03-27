2a6107a55f0a356ab888ff65c5f64e5b0a03ce03
Aryavart <mailaryavart@gmail.com>
2024-03-27 23:53:42 +0530

    updated

--- Python_Selenium/13_headless_mode.py
+++ Python_Selenium/13_headless_mode.py
@@ -36,3 +36,13 @@ driver.get("https://rahulshettyacademy.com/angularpractice/")
 
 print(driver.title) 
 """
+"""  
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("headless")
+chrome_options.add_argument("--ignore-certificate-errors")
+
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+
+print(driver.title) 
+"""
