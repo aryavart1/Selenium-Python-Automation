a7af90de4c0d3a3839daac3ac10792c1b41f208c
Aryavart <mailaryavart@gmail.com>
2024-04-08 00:38:15 +0530

    added E2E.py

---
+++ E2E/e2e.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+service_obj = Service()
+driver = webdriver.Chrome(options=options, service=service_obj)
+
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+
+driver.find_element(By.CLASS_NAME, "nav-link")
+
