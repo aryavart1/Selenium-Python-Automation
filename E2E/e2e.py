5a9ee3c8cc57c896b6ea5d558e7e080bf2c74797
Aryavart <mailaryavart@gmail.com>
2024-04-08 01:51:52 +0530

    updated

--- E2E/e2e.py
+++ E2E/e2e.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
+
+# chrome driver
 from selenium.webdriver.chrome.service import Service
+
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -9,7 +14,13 @@ service_obj = Service()
 driver = webdriver.Chrome(options=options, service=service_obj)
 
 driver.maximize_window()
+driver.implicitly_wait(4)
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 
-driver.find_element(By.CLASS_NAME, "nav-link")
+# XPath and CSS selector can also be used for partial values
+# Using regular expression CSS -> tagname[attribute*='value']
+# Using regular expression Xpath ->  //tagname[contains(@attribute,'value')]
+driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
+
+
 
