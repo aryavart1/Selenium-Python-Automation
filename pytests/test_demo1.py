7d6c2e457abd68e078cf28f1d344115d1a71a5a7
Aryavart <mailaryavart@gmail.com>
2024-05-04 00:21:10 +0530

    added pytests

---
+++ pytests/test_demo1.py
@@ -0,0 +1,6 @@
+# Pytest naming convention -> any Pytest file should start with test_ or end with _test
+# pytest method name should start with test
+# Any code should be wrapped in methods only
+
+def test_firstProgram():
+    print("Hello")
