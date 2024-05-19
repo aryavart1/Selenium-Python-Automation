4fe8696fc009b25a216d651a45f072335ca2fbaf
Aryavart <mailaryavart@gmail.com>
2024-05-20 02:31:14 +0530

    updated

--- Python_Selenium/2_Locators/Locators2.py
+++ Python_Selenium/2_Locators/Locators2.py
@@ -14,4 +14,5 @@ driver.maximize_window()  #maximize the browser window
 sliders=driver.find_elements(By.CLASS_NAME,'homeslider-container')
 print(len(sliders)) #5    total number of sliders on home page
 
-
+links=driver.find_elements(By.TAG_NAME,'a')
+print(len(links)) #  149 total number of links on home page
