5c671143760615de911d4342fb7882fd7869f633
Aryavart <mailaryavart@gmail.com>
2024-06-06 23:57:42 +0530

    updated

--- Selenium_Practice/8_Handling Date Pickers/datepicker2.py
+++ Selenium_Practice/8_Handling Date Pickers/datepicker2.py
@@ -3,8 +3,14 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
-serv_obj=Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
-driver=webdriver.Chrome(service=serv_obj)
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+service_obj = Service()
+driver = webdriver.Chrome(options=options, service=service_obj)
+
+'''serv_obj=Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+driver=webdriver.Chrome(service=serv_obj)'''
 
 driver.get("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
 
