556f00c6fbedcb83675eb67dc676f2d83e00c5bf
Aryavart <mailaryavart@gmail.com>
2024-06-06 23:56:59 +0530

    updated

--- Selenium_Practice/7_Handling Notification Popups & WebTables/notoficationpopup.py
+++ Selenium_Practice/7_Handling Notification Popups & WebTables/notoficationpopup.py
@@ -4,7 +4,14 @@ from selenium.webdriver.chrome.service import Service
 ops=webdriver.ChromeOptions()
 ops.add_argument("--disable-notificatins")
 
-serv_obj=Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+service_obj = Service()
+driver = webdriver.Chrome(options=options, service=service_obj)
+
+'''serv_obj=Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+driver=webdriver.Chrome(service=serv_obj)'''
 
 driver=webdriver.Chrome(service=serv_obj,options=ops)
 
