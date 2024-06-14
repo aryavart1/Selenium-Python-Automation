8afe2a202b155d3b2003f32f117ba088d78f7d0b
Aryavart <mailaryavart@gmail.com>
2024-06-14 22:20:28 +0530

    updated

--- Selenium_Practice/6_Working with Alerts, Frames & Browser Windows/inneriframes.py
+++ Selenium_Practice/6_Working with Alerts, Frames & Browser Windows/inneriframes.py
@@ -17,10 +17,10 @@ driver.maximize_window()
 
 driver.find_element(By.XPATH,"//a[normalize-space()='Iframe with in an Iframe']").click()
 
-outerframe=driver.find_element(By.XPATH,"//iframe[@src='MultipleFrames.html']")
+outerframe = driver.find_element(By.XPATH,"//iframe[@src='MultipleFrames.html']")
 driver.switch_to.frame(outerframe)
 
-innerframe=driver.find_element(By.XPATH,"/html/body/section/div/div/iframe")
+innerframe = driver.find_element(By.XPATH,"/html/body/section/div/div/iframe")
 driver.switch_to.frame(innerframe)
 
 driver.find_element(By.XPATH,"//input[@type='text']").send_keys("welcome")
