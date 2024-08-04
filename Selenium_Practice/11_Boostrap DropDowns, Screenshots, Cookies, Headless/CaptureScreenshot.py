d2e47b56883886934c4e6342a67e529cf39c3d6e
Aryavart <mailaryavart@gmail.com>
2024-08-05 00:33:52 +0530

    updated

--- Selenium_Practice/11_Boostrap DropDowns, Screenshots, Cookies, Headless/CaptureScreenshot.py
+++ Selenium_Practice/11_Boostrap DropDowns, Screenshots, Cookies, Headless/CaptureScreenshot.py
@@ -18,10 +18,10 @@ driver.implicitly_wait(10)
 driver.get("https://demo.nopcommerce.com/")
 driver.maximize_window()
 
-#driver.save_screenshot("C:\\Users\\admin\\PycharmProjects\\PythonSelenium\\day23\\homepage.png")
+# driver.save_screenshot("C:\\Users\\admin\\PycharmProjects\\PythonSelenium\\day23\\homepage.png")
 driver.save_screenshot(os.getcwd()+"\\homepage.png")
-#driver.get_screenshot_as_file(os.getcwd()+"\\homepage.png")
+# driver.get_screenshot_as_file(os.getcwd()+"\\homepage.png")
 
-#driver.get_screenshot_as_png()  #driver.get_screenshot_as_base64() #saves in binary format
+# driver.get_screenshot_as_png()  #driver.get_screenshot_as_base64() #saves in binary format
 
 driver.quit()
--- Selenium_Practice/11_Boostrap DropDowns, Screenshots, Cookies, Headless/HandleCookies.py
+++ Selenium_Practice/11_Boostrap DropDowns, Screenshots, Cookies, Headless/HandleCookies.py
@@ -15,29 +15,29 @@ driver.implicitly_wait(10)
 driver.get("https://demo.nopcommerce.com/")
 driver.maximize_window()
 
-#Capture Cookies from the browser
+# Capture Cookies from the browser
 cookies=driver.get_cookies()
 print("Size of cookies:",len(cookies)) #4
 
-#Print details of all cookies
+# Print details of all cookies
 # for c in cookies:
 #     #print(c)
 #     print(c.get('name'),":",c.get('value'))
 
 
-#Add new cookie to the browser
+# Add new cookie to the browser
 driver.add_cookie({"name":"MyCookie", "value":"123456"})
-cookies=driver.get_cookies()
+cookies = driver.get_cookies()
 print("Size of cookies after adding new one:",len(cookies)) #5
 
-#Delete specific cookie from the browser
+# Delete specific cookie from the browser
 driver.delete_cookie("MyCookie")
-cookies=driver.get_cookies()
+cookies = driver.get_cookies()
 print("Size of cookies after deleted one:",len(cookies)) #4
 
-#Delete all teh cookies
+# Delete all teh cookies
 driver.delete_all_cookies()
-cookies=driver.get_cookies()
+cookies = driver.get_cookies()
 print("Size of cookies after deleted all:",len(cookies)) #0
 
 driver.quit()
