0c5d7517344b111f1ea144a3a78f2324a4d82727
Aryavart <mailaryavart@gmail.com>
2024-08-15 18:16:16 +0530

    updated Pytest

--- pytests/test_demo1.py
+++ pytests/test_demo1.py
@@ -2,7 +2,14 @@
 # pytest method name should start with test
 # Any code should be wrapped in methods only
 # To run on command prompt -> py.test -v -s
+# To run a specific test file -> py.test <file name> -v -s
+# To run specific test methods -> py.test -k <method name or expression> -v -s
+# -k stands for method names execution, -s logs in output, -v stands for more info metadata
 
+import pytest
+
+
+@pytest.mark.smoke
 def test_firstProgram():
     message = "Hello"
-    assert message == "Hello", "Test Failed"
+    assert message == "Hello", "Test Failed" # If the TC gets failed it will print a different message
--- pytests/test_demo2.py
+++ pytests/test_demo2.py
@@ -1,3 +1,9 @@
-def test_firstProgram():
+@pytest.mark.smoke
+def test_FirstProgram():
     message = "Hello"
     assert message == "Hi", "Test Failed because strings do not match"
+
+def test_SecondProgram():
+    a = 2
+    b = 6
+    assert a+4 == b, "Addition doesn't match"
