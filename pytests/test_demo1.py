22a47a83fc5515c7c400b1726ea02fc8e540545a
Aryavart <mailaryavart@gmail.com>
2024-08-16 00:07:51 +0530

    added fixtures

--- pytests/test_demo1.py
+++ pytests/test_demo1.py
@@ -5,11 +5,38 @@
 # To run a specific test file -> py.test <file name> -v -s
 # To run specific test methods -> py.test -k <method name or expression> -v -s
 # -k stands for method names execution, -s logs in output, -v stands for more info metadata
+# To run testcases that are marked (tagged) -> py.test -m <tag_name> -v -s
+# you can skip tests with @pytest.mark.skip
+# @pytest.mark.xfail -> to run TC without displaying output
+# fixtures are used as setup and tear down methods for test cases- conftest file to generalize fixt
+# fixture and make it available to all test cases (fixture name into parameters of method)
+# datadriven and parameterization can be done with return statements in tuple format
+# when you define fixture scope to class only, it will run once before class is initiated and at the end
 
 import pytest
-
-
 @pytest.mark.smoke
+def test_firstProgram(setup):
+    print("Hello")
+
+@pytest.mark.xfail
+def test_SecondGreetCreditCard():
+    print("Good Morning")
+
+def test_crossBrowser(crossBrowser):
+    print(crossBrowser[1])
+
+
+"""
+import pytest
+@pytest.mark.smoke  # will run Testcases which are marked as Smoke
+@pytest.mark.skip   # to skip particular test case
 def test_firstProgram():
     message = "Hello"
-    assert message == "Hello", "Test Failed" # If the TC gets failed it will print a different message
+    assert message == "Hello", "Test Failed"  # If the TC gets failed it will print a different message
+
+
+@pytest.mark.xfail   # tc will run but will not be displayed in output
+def test_SecondProgram():
+    message = "Hello"
+    assert message == "Hello", "Test Failed"
+"""
--- pytests/test_demo2.py
+++ pytests/test_demo2.py
@@ -1,4 +1,20 @@
+import pytest
+
+
 @pytest.mark.smoke
+# @pytest.mark.skip
+def test_firstProgram():
+    msg = "Hello"  # operations
+    assert msg == "Hi", "Test failed because strings do not match"
+
+
+def test_SecondCreditCard():
+    a = 4
+    b = 6
+    assert a + 2 == 6, "Addition do not match"
+
+
+"""@pytest.mark.smoke
 def test_FirstProgram():
     message = "Hello"
     assert message == "Hi", "Test Failed because strings do not match"
@@ -7,3 +23,4 @@ def test_SecondProgram():
     a = 2
     b = 6
     assert a+4 == b, "Addition doesn't match"
+"""
