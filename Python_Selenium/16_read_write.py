47f4f738e28632a88cfade305eef210c29ddb5d9
Aryavart <mailaryavart@gmail.com>
2025-07-25 20:18:34 +0530

    updated

--- Python_Selenium/16_read_write.py
+++ Python_Selenium/16_read_write.py
@@ -1,5 +1,14 @@
 file = open('C:/Users/maila/Selenium_Python1/readWrite.txt')
 
-print(file.read(8))
+# read n number of character by passing the parameter
+# print(file.read(8))
+
+# read one single line at a time -> readline
+# print(file.readline())
+
+line = file.readline()
+while line != "":
+    print(line)
+    line = file.readline()
 
 file.close()
